ba5c296e8ebeddac6b9db425599e0564684db12b
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2016-11-23 21:50:11 +0800

    Refine test.sh, add clean_env and check statements for test case one.

--- src/test.sh
+++ src/test.sh
@@ -11,10 +11,62 @@ function assert_status ()
     fi
 }
 
+function assert_condition ()
+{
+    E_PARAM_ERR=98
+    E_ASSERT_FAILED=99
+
+
+    if [ -z "$2" ]          #  Not enough parameters passed
+    then                    #+ to assert() function.
+        return $E_PARAM_ERR #  No damage done.
+    fi
+
+    lineno=$2
+
+    if [ ! $1 ]
+    then
+        echo "Assertion failed:  \"$1\""
+        echo "File \"$0\", line $lineno"    # Give name of file and line number.
+        exit $E_ASSERT_FAILED
+        # else
+        #   return
+        #   and continue executing the script.
+    fi
+}
+
+function clean_env () {
+    # test one
+    rm -rf ./test/one/test_DP_output_alltag_V13
+    rm -rf ./test/one/test_DP_output_kws_V10
+    rm -rf ./test/one/test_DP_output_main
+    rm -rf ./test/one/test_nrot_input_alltag_V13
+    rm -rf ./test/one/test_nrot_input_kws_V10
+    rm -rf ./test/one/test_upload
+    rm -rf ./test/one/test_merger
+
+    # test two
+}
+
+# clean env
+clean_env
+
 # test one
 echo "test one"
 /da1/db/python27/bin/python make-partitions.py ./test/test-one.conf
 assert_status "test one make-partitions.py"
-/da1/db/python27/bin/python merge-partitions.py ./test/test-one.conf
-assert_status "test one merge-partitions.py"
 
+assert_condition "-d ./test/one/test_nrot_input_main"
+assert_condition "-d ./test/one/test_DP_output_main"
+diff -r ./test/one/test_nrot_input_main ./test/one-gold/test_nrot_input_main
+assert_status "./test/one/test_nrot_input_main ./test/one-gold/test_nrot_input_main different"
+diff -r ./test/one/test_DP_output_main ./test/one-gold/test_DP_output_main
+assert_status "./test/one/test_DP_output_main ./test/one-gold/test_DP_output_main different"
+
+/da1/db/python27/bin/python merge-partitions.py ./test/test-one.conf
+assert_status "test one merge-partitions.py" assert_condition "-d
+./test/one/test_upload" assert_condition "-d ./test/one/test_merger"
+assert_status "$(ls ./test/one/test_merger) -eq 4"
+assert_status "$(ls ./test/one/test_upload) -eq 6"
+
+# test two
