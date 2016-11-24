f116460c836f925d8521c2b51cc0c15de80a343c
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2016-11-24 16:43:36 +0800

    Add model answer for test case one, one-gold.

---
+++ src/test/one-gold/test_upload/20161123193503.d
@@ -0,0 +1,6 @@
+sfun_明星八卦	http://www.yidianzixun.com/article/0EzkC7Oc
+cfun_8:娱乐:1:40	http://www.yidianzixun.com/article/0EzkC7Oc
+cfun_7:娱乐:1:40	http://www.yidianzixun.com/article/0EzkC7Oc
+kfun_王子文	http://www.yidianzixun.com/article/0EzkC7Oc
+tfun	http://www.yidianzixun.com/article/0EzkC7Oc
+kfun_欢乐颂	http://www.yidianzixun.com/article/0EzkC7Oc
